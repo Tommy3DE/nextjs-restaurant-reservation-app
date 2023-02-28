2a819218982aa1e841a06e5a7a204ec5d2085a4d
Tommy3DE <84966413+Tommy3DE@users.noreply.github.com>
2023-02-28 13:59:27 +0100

    dodanie filtracji

--- app/search/components/SearchSideBar.tsx
+++ app/search/components/SearchSideBar.tsx
@@ -1,37 +1,86 @@
-import { Cuisine, Location } from '@prisma/client'
-import React from 'react'
+import { Cuisine, Location, PRICE } from "@prisma/client";
+import React from "react";
+import Link from "next/link";
 
-const SearchSideBar = ({locations, cuisines}: {locations: Location[], cuisines: Cuisine[]}) => {
+const SearchSideBar = ({
+  locations,
+  cuisines,
+  searchParams,
+}: {
+  locations: Location[];
+  cuisines: Cuisine[];
+  searchParams: { city?: string; cuisine?: string; price?: PRICE };
+}) => {
+
+  const prices = [{
+    price: PRICE.CHEAP,
+    label: '$$'
+  }, {
+    price: PRICE.REGULAR,
+    label: '$$$'
+  }, {
+    price: PRICE.EXPENSIVE,
+    label: '$$$$'
+  }]
   return (
     <div className="w-1/5">
-            <div className="border-b pb-4">
-              <h1 className="mb-2">Region</h1>
-              {locations.map(location=>(
-              <p className="font-light text-reg">{location.name}</p>
-              ))}
-            </div>
-            <div className="border-b pb-4 mt-3">
-              <h1 className="mb-2">Cuisine</h1>
-              <p className="font-light text-reg">Mexican</p>
-              <p className="font-light text-reg">Italian</p>
-              <p className="font-light text-reg">Chinese</p>
-            </div>
-            <div className="mt-3 pb-4">
-              <h1 className="mb-2">Price</h1>
-              <div className="flex">
-                <button className="border w-full text-reg font-light rounded-l p-2">
-                  $
-                </button>
-                <button className="border-r border-t border-b w-full text-reg font-light p-2">
-                  $$
-                </button>
-                <button className="border-r border-t border-b w-full text-reg font-light p-2 rounded-r">
-                  $$$
-                </button>
-              </div>
-            </div>
-          </div>
-  )
-}
+      <div className="border-b pb-4 mt-3 flex flex-col">
+        <h1 className="mb-2">Region</h1>
+        {locations.map((location) => (
+          <Link
+            href={{
+              pathname: "/search",
+              query: {
+                ...searchParams,
+                city: location.name,
+              },
+            }}
+            className="font-light text-reg capitalize"
+            key={location.id}
+          >
+            {location.name}
+          </Link>
+        ))}
+      </div>
+      <div className="border-b pb-4 mt-3 flex flex-col">
+        <h1 className="mb-2">Cuisine</h1>
+        {cuisines.map((cuisine) => (
+          <Link
+            href={{
+              pathname: "/search",
+              query: {
+                ...searchParams,
+                cuisine: cuisine.name,
+              },
+            }}
+            className="font-light text-reg capitalize"
+            key={cuisine.id}
+          >
+            {cuisine.name}
+          </Link>
+        ))}
+      </div>
+      <div className="mt-3 pb-4">
+        <h1 className="mb-2">Price</h1>
+        <div className="flex">
+          {prices.map(price=>(
+          <Link
+            href={{
+              pathname: "/search",
+              query: {
+                ...searchParams,
+                price: price.price,
+              },
+            }}
+            className="border w-full text-reg font-light rounded-l p-2"
+          >
+            {price.label}
+          </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default SearchSideBar
+export default SearchSideBar;
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PRICE, PrismaClient } from "@prisma/client";
 import React from "react";
 import Header from "./components/Header";
 import RestaurantCard from "./components/RestaurantCard";
@@ -41,7 +41,7 @@ const fetchCuisine = async () =>{
   return prisma.cuisine.findMany()
 }
 
-const Search = async ({searchParams}: {searchParams: {city: string}}) => {
+const Search = async ({searchParams}: {searchParams: {city?: string, cuisine?: string, price?: PRICE}}) => {
   const restaurants = await fetchRestaurantByCity(searchParams.city)
   const locations = await fetchLocations()
   const cuisines = await fetchCuisine()
@@ -50,7 +50,7 @@ const Search = async ({searchParams}: {searchParams: {city: string}}) => {
     <>
       <Header />
       <div className="flex py-4 m-auto w-2/3 justify-between items-start">
-        <SearchSideBar locations={locations} cuisines={cuisines}/>
+        <SearchSideBar locations={locations} cuisines={cuisines} searchParams={searchParams}/>
         <div className="w-5/6">
           {restaurants.length ? (
             <>
