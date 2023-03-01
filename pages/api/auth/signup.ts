964e9f01b90fdd4ba110763cb77791c703cb9d89
Tommy3DE <84966413+Tommy3DE@users.noreply.github.com>
2023-03-01 14:26:20 +0100

    dodanie walidacji i ochrony danych

---
+++ pages/api/auth/signup.ts
@@ -0,0 +1,84 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import validator from 'validator'
+import { PrismaClient } from "@prisma/client";
+import bcrypt from 'bcrypt'
+
+const prisma = new PrismaClient()
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse){
+    if(req.method === 'POST'){
+        const {firstName, lastName, phone, email, password, city} = req.body
+        const errors: string[] =[]
+        const validationSchema = [
+            {
+                valid: validator.isLength(firstName, {
+                    min: 3,
+                    max: 20
+                }),
+                errorMessage: "Name is invalid"
+            },
+            {
+                valid: validator.isLength(lastName, {
+                    min: 2,
+                    max: 20
+                }),
+                errorMessage: "Surname is invalid"
+            },
+            {
+                valid: validator.isEmail(email),
+                errorMessage: "Email is invalid"
+            },
+            {
+                valid: validator.isMobilePhone(phone),
+                errorMessage: "Phone number is invalid"
+            },
+            {
+                valid: validator.isLength(city, {
+                    min: 3
+                }),
+                errorMessage: "City is invalid"
+            },
+            {
+                valid: validator.isStrongPassword(password),
+                errorMessage: "Password is too weak"
+            }
+        ];
+
+        validationSchema.forEach((check)=>{
+            if(!check.valid){
+                errors.push(check.errorMessage)
+            }
+        })
+
+        if(errors.length){
+            return res.status(400).json({errorMessage: errors[0]})
+        }
+
+        const userWithEmail= await prisma.user.findUnique({
+            where: {
+                email
+            }
+        })
+
+        if(userWithEmail){
+            return res.status(400).json({errorMessage: 'Email already used'})
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+        const user = await prisma.user.create({
+            data: {
+                first_name: firstName,
+                last_name: lastName,
+                password: hashedPassword,
+                city,
+                phone,
+                email
+            }
+        })
+
+        res.status(200).json({
+            hello: user
+        })
+    }
+}
