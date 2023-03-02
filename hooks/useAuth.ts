76ce39adffa2d022df40064d2819a3ddbec30fe8
Tommy3DE <84966413+Tommy3DE@users.noreply.github.com>
2023-03-02 11:26:48 +0100

    zmiany w modalu i rozwiniecie logowania i rejestracji

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -1,42 +1,97 @@
-import axios from "axios"
+import axios from "axios";
 import { useContext } from "react";
 import { AuthenticationContext } from "../app/context/AuthContext";
 
 const useAuth = () => {
+  const { data, error, loading, setAuthState } = useContext(
+    AuthenticationContext
+  );
 
-    const {data, error, loading, setAuthState} = useContext(AuthenticationContext)
-
-    const signin = async ({email, password}: {email: string; password: string})=> {
-        setAuthState({
-            data: null,
-            error: null,
-            loading: true
-        })
-        try {
-            const response = await axios.post('http://localhost:3000/api/auth/signin', {
-                email,
-                password
-            })
-            console.log(response)
-            setAuthState({
-                data: response.data,
-                error: null,
-                loading: false
-            })
-        } catch (error: any) {
-            setAuthState({
-                data: null,
-                error: error.response.data.errorMessage,
-                loading: false
-            })
+  const signin = async (
+    { email, password }: { email: string; password: string },
+    handleClose: () => void
+  ) => {
+    setAuthState({
+      data: null,
+      error: null,
+      loading: true,
+    });
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/api/auth/signin",
+        {
+          email,
+          password,
         }
+      );
+      console.log(response);
+      setAuthState({
+        data: response.data,
+        error: null,
+        loading: false,
+      });
+      handleClose();
+    } catch (error: any) {
+      setAuthState({
+        data: null,
+        error: error.response.data.errorMessage,
+        loading: false,
+      });
     }
-    const signup = async ()=> {
-
-    }
-    return {
-        signin,
-        signup
-    }
-}
-export default useAuth
+  };
+  const signup = async (
+    {
+      email,
+      password,
+      firstName,
+      lastName,
+      city,
+      phone,
+    }: {
+      email: string;
+      password: string;
+      firstName: string;
+      lastName: string;
+      city: string;
+      phone: string;
+    },
+    handleClose: () => void
+  ) => {
+    setAuthState({
+        data: null,
+        error: null,
+        loading: true,
+      });
+      try {
+        const response = await axios.post(
+          "http://localhost:3000/api/auth/signup",
+          {
+            email,
+            password,
+            firstName,
+            lastName,
+            city,
+            phone,
+          }
+        );
+        console.log(response);
+        setAuthState({
+          data: response.data,
+          error: null,
+          loading: false,
+        });
+        handleClose();
+      } catch (error: any) {
+        setAuthState({
+          data: null,
+          error: error.response.data.errorMessage,
+          loading: false,
+        });
+      }
+  };
+  return {
+    signin,
+    signup,
+  };
+};
+export default useAuth;
--- pages/api/auth/signin.ts
+++ pages/api/auth/signin.ts
@@ -3,6 +3,7 @@ import validator from "validator";
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
 import * as jose from "jose";
+import {setCookie} from 'cookies-next'
 
 
 const prisma = new PrismaClient()
@@ -34,15 +35,15 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     if(errors.length){
         return res.status(400).json({errorMessage: errors[0]})
     }
-    const userWithEmail = await prisma.user.findUnique({
+    const user = await prisma.user.findUnique({
         where: {
             email
         }
     })
-    if(!userWithEmail){
+    if(!user){
         return res.status(401).json({errorMessage: 'Email or password not valid'})
     }
-    const isMatch = await bcrypt.compare(password, userWithEmail.password)
+    const isMatch = await bcrypt.compare(password, user.password)
 
     if(!isMatch){
         res.status(401).json({errorMessage: 'Email or password not valid'})
@@ -51,13 +52,20 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const alg = "HS256";
     const secret = new TextEncoder().encode(process.env.JWT_SECRET);
 
-    const token = await new jose.SignJWT({ email: userWithEmail.email })
+    
+    const token = await new jose.SignJWT({ email: user.email })
       .setProtectedHeader({ alg })
       .setExpirationTime("24h")
       .sign(secret);
 
+      setCookie('jwt', token, {req, res, maxAge: 60*6*24})
+
     return res.status(200).json({
-      token: token,
+      firstName: user.first_name,
+      lastName: user.last_name,
+      email: user.email,
+      phone: user.phone,
+      city: user.city,
     });
   }
   return res.status(404).json("Undefined endpoint");
