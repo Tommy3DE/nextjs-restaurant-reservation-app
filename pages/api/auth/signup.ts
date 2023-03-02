39405c925555e1dd4bedcf94b499519d8e0ea6c3
Tommy3DE <84966413+Tommy3DE@users.noreply.github.com>
2023-03-02 07:59:54 +0100

    zmiany w fetchu

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -1,84 +1,97 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import validator from 'validator'
+import validator from "validator";
 import { PrismaClient } from "@prisma/client";
-import bcrypt from 'bcrypt'
+import bcrypt from "bcrypt";
+import * as jose from "jose";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse){
-    if(req.method === 'POST'){
-        const {firstName, lastName, phone, email, password, city} = req.body
-        const errors: string[] =[]
-        const validationSchema = [
-            {
-                valid: validator.isLength(firstName, {
-                    min: 3,
-                    max: 20
-                }),
-                errorMessage: "Name is invalid"
-            },
-            {
-                valid: validator.isLength(lastName, {
-                    min: 2,
-                    max: 20
-                }),
-                errorMessage: "Surname is invalid"
-            },
-            {
-                valid: validator.isEmail(email),
-                errorMessage: "Email is invalid"
-            },
-            {
-                valid: validator.isMobilePhone(phone),
-                errorMessage: "Phone number is invalid"
-            },
-            {
-                valid: validator.isLength(city, {
-                    min: 3
-                }),
-                errorMessage: "City is invalid"
-            },
-            {
-                valid: validator.isStrongPassword(password),
-                errorMessage: "Password is too weak"
-            }
-        ];
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    const { firstName, lastName, phone, email, password, city } = req.body;
+    const errors: string[] = [];
+    const validationSchema = [
+      {
+        valid: validator.isLength(firstName, {
+          min: 3,
+          max: 20,
+        }),
+        errorMessage: "Name is invalid",
+      },
+      {
+        valid: validator.isLength(lastName, {
+          min: 2,
+          max: 20,
+        }),
+        errorMessage: "Surname is invalid",
+      },
+      {
+        valid: validator.isEmail(email),
+        errorMessage: "Email is invalid",
+      },
+      {
+        valid: validator.isMobilePhone(phone),
+        errorMessage: "Phone number is invalid",
+      },
+      {
+        valid: validator.isLength(city, {
+          min: 3,
+        }),
+        errorMessage: "City is invalid",
+      },
+      {
+        valid: validator.isStrongPassword(password),
+        errorMessage: "Password is too weak",
+      },
+    ];
 
-        validationSchema.forEach((check)=>{
-            if(!check.valid){
-                errors.push(check.errorMessage)
-            }
-        })
+    validationSchema.forEach((check) => {
+      if (!check.valid) {
+        errors.push(check.errorMessage);
+      }
+    });
 
-        if(errors.length){
-            return res.status(400).json({errorMessage: errors[0]})
-        }
-
-        const userWithEmail= await prisma.user.findUnique({
-            where: {
-                email
-            }
-        })
-
-        if(userWithEmail){
-            return res.status(400).json({errorMessage: 'Email already used'})
-        }
-
-        const hashedPassword = await bcrypt.hash(password, 10)
-
-        const user = await prisma.user.create({
-            data: {
-                first_name: firstName,
-                last_name: lastName,
-                password: hashedPassword,
-                city,
-                phone,
-                email
-            }
-        })
-
-        res.status(200).json({
-            hello: user
-        })
+    if (errors.length) {
+      return res.status(400).json({ errorMessage: errors[0] });
     }
+
+    const userWithEmail = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (userWithEmail) {
+      return res.status(400).json({ errorMessage: "Email already used" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        first_name: firstName,
+        last_name: lastName,
+        password: hashedPassword,
+        city,
+        phone,
+        email,
+      },
+    });
+
+    const alg = "HS256";
+    const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+
+    const token = await new jose.SignJWT({ email: user.email })
+      .setProtectedHeader({ alg })
+      .setExpirationTime("24h")
+      .sign(secret);
+
+    return res.status(200).json({
+      token: token,
+    });
+  }
+  return res.status(404).json('Undefined endpoint')
 }
