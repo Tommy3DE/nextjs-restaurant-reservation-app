c834e7c5651af2de7434c72c96e8707372b7d5f9
Tommy3DE <84966413+Tommy3DE@users.noreply.github.com>
2023-03-02 10:50:37 +0100

    dodanie contextu i error management

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -1,6 +1,35 @@
-const useAuth = () => {
-    const signin = async ()=> {
+import axios from "axios"
+import { useContext } from "react";
+import { AuthenticationContext } from "../app/context/AuthContext";
 
+const useAuth = () => {
+
+    const {data, error, loading, setAuthState} = useContext(AuthenticationContext)
+
+    const signin = async ({email, password}: {email: string; password: string})=> {
+        setAuthState({
+            data: null,
+            error: null,
+            loading: true
+        })
+        try {
+            const response = await axios.post('http://localhost:3000/api/auth/signin', {
+                email,
+                password
+            })
+            console.log(response)
+            setAuthState({
+                data: response.data,
+                error: null,
+                loading: false
+            })
+        } catch (error: any) {
+            setAuthState({
+                data: null,
+                error: error.response.data.errorMessage,
+                loading: false
+            })
+        }
     }
     const signup = async ()=> {
 
